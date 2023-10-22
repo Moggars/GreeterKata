aac131ab994f5966462b6a34c155f3458ed0b418
Dale Morgan <d.morgan93@outlook.com>
2023-10-22 14:49:22 +0100

    Task 1: TestCase

--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class GreeterTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GreeterTest {
+
+    @Test
+    public void greetReturnsHelloName(){
+        Greeter greeter = new Greeter();
+        String name = "Dale";
+        String expectedOutput = "Hello Dale";
+        String notExpectedOutput ="Hello ";
+
+        String greeting = greeter.greet(name);
+
+        assertNotNull(greeting);
+        assertEquals(expectedOutput,greeting);
+        assertNotEquals(notExpectedOutput,greeting);
+    }
 }
+
