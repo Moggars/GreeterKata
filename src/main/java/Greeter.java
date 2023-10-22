22fec2d24cb42eb3e2750b39fc7c95aa549d66a0
Dale Morgan <d.morgan93@outlook.com>
2023-10-22 16:09:16 +0100

    Task 2: greet trims the name input

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -2,6 +2,7 @@ public class Greeter {
 
     public String greet(String name){
         final String hello = "Hello ";
+        name = name.trim();
         String output = hello + name;
         return output;
     }
