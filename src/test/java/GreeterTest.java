00a47b7d21619a788c77d7475189e173947f9f06
Dale Morgan <d.morgan93@outlook.com>
2023-10-22 16:03:56 +0100

    Task 2: Added TestCase for trim and refactored test class

--- src/test/java/GreeterTest.java
+++ src/test/java/GreeterTest.java
@@ -1,21 +1,37 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GreeterTest {
 
+    private Greeter greeter;
+    private final String expectedOutput = "Hello Dale";
+    @BeforeEach
+    public void setUp() {
+        greeter = new Greeter();
+    }
+
     @Test
-    public void greetReturnsHelloName(){
-        Greeter greeter = new Greeter();
+    public void testGreetReturnsHelloName() {
         String name = "Dale";
-        String expectedOutput = "Hello Dale";
-        String notExpectedOutput ="Hello ";
+        String notExpectedOutput = "Hello ";
 
         String greeting = greeter.greet(name);
 
         assertNotNull(greeting);
-        assertEquals(expectedOutput,greeting);
-        assertNotEquals(notExpectedOutput,greeting);
+        assertEquals(this.expectedOutput, greeting);
+        assertNotEquals(notExpectedOutput, greeting);
+    }
+
+    @Test
+    public void testGreetReturnsHelloNameTrimmed() {
+        String name = " Dale ";
+        String notExpectedOutput = "Hello  Dale ";
+
+        String greeting = greeter.greet(name);
+
+        assertNotNull(greeting);
+        assertNotEquals(notExpectedOutput, greeting);
+        assertEquals(expectedOutput, greeting);
     }
 }
-
