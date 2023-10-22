dd787371133f703beebd704f840b7ab92e279ca9
Dale Morgan <d.morgan93@outlook.com>
2023-10-22 14:59:48 +0100

    Task 1: Added greet

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Greeter {
+public class Greeter {
+
+    public String greet(String name){
+        final String hello = "Hello ";
+        String output = hello + name;
+        return output;
+    }
 }
