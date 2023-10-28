5c634ff564c2f147c39015f42aed219b34d7637b
Dale Morgan <d.morgan93@outlook.com>
2023-10-28 17:24:15 +0100

    Task 3: Greet Capitalises First Letter Of Name

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -3,7 +3,12 @@ public class Greeter {
     public String greet(String name){
         final String hello = "Hello ";
         name = name.trim();
+        name = capitaliseFirstLetter(name);
         String output = hello + name;
         return output;
+    }
+
+    public String capitaliseFirstLetter(String name){
+        return name.substring(0,1).toUpperCase() + name.substring(1);
     }
 }
